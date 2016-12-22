b0cf2d30721cd5f6e89df832b3e563f6c58fd04e
bpshoe <bpshoe@gmail.com>
2016-12-22 12:40:52 -0500

    try different json format

--- thermometer.py
+++ thermometer.py
@@ -13,29 +13,28 @@ kinesis = boto3.client('kinesis')
 
 def read_temp_raw():
     f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
+lines = f.readlines()
+f.close()
+return lines
 
 def read_temp():
     lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+while lines[0].strip()[-3: ] != 'YES':
+    time.sleep(0.2)
+lines = read_temp_raw()
+equals_pos = lines[1].find('t=')
+if equals_pos != -1:
+    temp_string = lines[1][equals_pos + 2: ]
+temp_c = float(temp_string) / 1000.0
+temp_f = temp_c * 9.0 / 5.0 + 32.0
+return temp_c, temp_f
 
-#figure out why all of my indents are crap
-    data = {}
-	data['temp_c'] = temp_c
-	data['sensor'] = "weatherpi"
-	json_data = json.dumps(data)
-    # kinesis.put_record(StreamName="thermometer", Data=json_data, PartitionKey="weatherpi")
+# figure out why all of my indents are crap
+data = {}
+data['temp_c'] = temp_c
+data['sensor'] = "weatherpi"
+json_data = json.dumps(data)# kinesis.put_record(StreamName = "thermometer", Data = json_data, PartitionKey = "weatherpi")
 
 while True:
-	print(read_temp())	
-	time.sleep(1)
+    print(read_temp())
+time.sleep(1)
