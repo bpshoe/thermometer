d0f63adb5c8ff673f7638b8c35b64dea18156452
bpshoe <bpshoe@gmail.com>
2016-12-22 12:15:09 -0500

    moved more indentsgit add *

--- thermometer.py
+++ thermometer.py
@@ -31,8 +31,8 @@ def read_temp():
 
 #figure out why all of my indents are crap
     data = {}
-		data['temp_c'] = temp_c
-		data['sensor'] = "weatherpi"
+	data['temp_c'] = temp_c
+	data['sensor'] = "weatherpi"
 	json_data = json.dumps(data)
     # kinesis.put_record(StreamName="thermometer", Data=json_data, PartitionKey="weatherpi")
 
