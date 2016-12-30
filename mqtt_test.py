cdb9b2b70604ee772140f383586acefa903edeff
bpshoe <bpshoe@gmail.com>
2016-12-29 19:18:48 -0500

    pulled in thermometer code

--- mqtt_test.py
+++ mqtt_test.py
@@ -20,6 +20,33 @@ import sys
 import logging
 import time
 import getopt
+import os
+import glob
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+    return temp_c, temp_f
 
 # Custom MQTT message callback
 def customCallback(client, userdata, message):
@@ -137,6 +164,6 @@ time.sleep(2)
 # Publish to the same topic in a loop forever
 loopCount = 0
 while True:
-	myAWSIoTMQTTClient.publish("sdk/test/Python", "Brian's New Message " + str(loopCount), 1)
+	myAWSIoTMQTTClient.publish("sdk/test/Python", print(read_temp()), 1)
 	loopCount += 1
 	time.sleep(1)
